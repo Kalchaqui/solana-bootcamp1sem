e89b5a248e74b131ab3d5dc54008a495188779ca
Kalcha <diego.rbarrionuevo@gmail.com>
2024-02-22 14:40:19 -0300

    bob

---
+++ src/app/transfer-modal.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit, TemplateRef } from "@angular/core";
+
+                            Component({
+                            selector: 'bob-transfer-modal',
+                            template:  `<h2> Tramsferir Fondos </h2> `,
+                            standalone: true
+                            })
+
+                            export class TransferModalComponent {};
